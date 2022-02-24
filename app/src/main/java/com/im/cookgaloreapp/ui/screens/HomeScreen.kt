b478b6e3f12b8361a10d2ca6827e49c55544696c
ismail <ismail1973x@gmail.com>
2022-02-24 06:53:20 +0000

    lato fonts added

--- app/src/main/java/com/im/cookgaloreapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/im/cookgaloreapp/ui/screens/HomeScreen.kt
@@ -7,7 +7,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.im.cookgaloreapp.ui.theme.CookGaloreAppTheme
 
 
 @Composable
@@ -16,9 +24,30 @@ fun HomeScreen(
 {
     Column(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Cook Galore")
+        Column {
+            Text(
+                text = "Discover the best recipes",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 22.sp,
+                ),
+            )
+            Text(
+                text = "for cooking",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 22.sp,
+                ),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    CookGaloreAppTheme {
+        HomeScreen(navController = rememberNavController())
     }
 }
--- app/src/main/java/com/im/cookgaloreapp/ui/theme/Type.kt
+++ app/src/main/java/com/im/cookgaloreapp/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.im.cookgaloreapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.im.cookgaloreapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -25,4 +27,18 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+)
+
+val Fonts = FontFamily(
+//    Font(R.font.lato_black),
+//    Font(R.font.lato_blackitalic),
+    Font(R.font.lato_bold, weight = FontWeight.Bold),
+//    Font(R.font.lato_bolditalic, weight = FontWeight.Bold, style = FontStyle.Italic),
+//    Font(R.font.lato_italic, style = FontStyle.Italic),
+//    Font(R.font.lato_light, weight = FontWeight.Light),
+//    Font(R.font.lato_lightitalic, weight = FontWeight.Light, style = FontStyle.Italic),
+//    Font(R.font.lato_regular, weight = FontWeight.Normal),
+//    Font(R.font.lato_thin, weight = FontWeight.Thin),
+//    Font(R.font.lato_thinitalic, weight = FontWeight.Thin, style = FontStyle.Italic)
+
 )
